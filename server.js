a8113f6fc8c9c9dbfbc06bd19ade58b9adc4ccb8
Satyajeet Singh <satyajeet104@gmail.com>
2018-03-27 12:55:25 +0530

    Basic db.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const path = require('path')
-
+const db = require('./db');
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+
+app.get('/',(req,res)=>{
+  
+    db.connect();
+    
+    db.query('SELECT 1 + 1 from dual', function (error, results, fields) {
+      if (error) throw error;
+      console.log('The solution is: ', results);
+    });
+    
+   
+})
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(2678, () => console.log('Server started at http://localhost:2678'))
