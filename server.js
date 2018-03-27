eace40d12a0581ae2dff57d6c22c1de9d6267ef6
Satyajeet Singh <satyajeet104@gmail.com>
2018-03-27 13:11:18 +0530

    Basic admin.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const db = require('./db');
+const admin = require('./routes/admin')
 const app = express();
 
 app.use(express.json())
@@ -14,10 +15,10 @@ app.get('/',(req,res)=>{
     db.query('SELECT 1 + 1 from dual', function (error, results, fields) {
       if (error) throw error;
       console.log('The solution is: ', results);
-    });
-    
+    })   
    
 })
+app.use('/admin',admin);
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(2678, () => console.log('Server started at http://localhost:2678'))
