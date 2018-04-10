a72f755030332494469f19cce8743430da536905
Satyajeet Singh <satyajeet104@gmail.com>
2018-04-10 21:48:08 +0530

    query.js

--- api/routes/query.js
+++ api/routes/query.js
@@ -12,7 +12,8 @@ router.post('/totalClasses', (req, res,next) => {
     var finalAns={};
 
    
-    var sid = parseInt(req.user);
+    var sid = 1;
+    
    // var date= req.body.date;
    // var otp=req.body.otp;
    var sql11='select class from student_details where student_id=?'
@@ -77,108 +78,111 @@ var sql1 = 'select distinct attendance_date  from  attendance'+Class+' where '+s
                console.log('The solution is: ', results);
                console.log(results.length);
                finalAns.sub5=results.length;
-res.send(finalAns);
+
+
+               var sql11='select class from student_details where student_id=?'
+               db.query(sql11,[sid], function (error, results, fields) {
                
-
-           })
-        
-
-
-         })
-
-
-       })
-
-
-
-
-     })
-       
-
-   })
-})
-
-
-
-   })
-
-   
-
-})
-
-
-
-router.post('/totalStudentClasses', (req, res,next) => {
-
-    var finalAns={};
-
-   
-    var sid = parseInt(req.user);
-   // var date= req.body.date;
-   // var otp=req.body.otp;
-   var sql11='select class from student_details where student_id=?'
-   db.query(sql11,[sid], function (error, results, fields) {
-   
-   if (error) throw error;
-   console.log('The solution is: ', results);
-   Class=results[0].class;   
-   
-   
-   var sql = `select * from classes where class=?;`;
-console.log("classasas"+Class);
-
-   db.query(sql,[Class], function (error, results, fields) {
-     if (error) throw error;
-     console.log('The solution is: ', results);
-
-     var subject1=results[0].subject1;
-     var subject2=results[0].subject2;
-     var subject3=results[0].subject3;
-     var subject4=results[0].subject4;
-     var subject5=results[0].subject5;
-
-console.log("subjects"+subject1+subject2+subject3);
-
-var sql1 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject1+' = ? and student_id ='+sid+';'
-   
-     db.query(sql1,['P',sid], function (error, results, fields) {
-       if (error) throw error;
-       console.log('The solution is: ', results);
-       console.log(results.length);
-       finalAns.sub1=results.length;
-
-       var sql2 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject2+' <> ? and student_id ='+sid+';'
-   
-       db.query(sql2,['N'], function (error, results, fields) {
-         if (error) throw error;
-         console.log('The solution is: ', results);
-         console.log(results.length);
-         finalAns.sub2=results.length;
-
-         var sql3 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject3+' <> ? and student_id ='+sid+';'
-   
-         db.query(sql3,['N'], function (error, results, fields) {
-           if (error) throw error;
-           console.log('The solution is: ', results);
-           console.log(results.length);
-           finalAns.sub3=results.length;
-
-           var sql4 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject4+' <> ? and student_id ='+sid+';'
-   
-           db.query(sql4,['N'], function (error, results, fields) {
-             if (error) throw error;
-             console.log('The solution is: ', results);
-             console.log(results.length);
-             finalAns.sub4=results.length;
-
-             var sql5 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject5+' <> ? and student_id ='+sid+';'
-   
-             db.query(sql5,['N'], function (error, results, fields) {
                if (error) throw error;
                console.log('The solution is: ', results);
-               console.log(results.length);
-               finalAns.sub5=results.length;
-res.send(finalAns);
+               Class=results[0].class;   
+               
+               
+               var sql = `select * from classes where class=?;`;
+            console.log("classasas"+Class);
+            
+               db.query(sql,[Class], function (error, results, fields) {
+                 if (error) throw error;
+                 console.log('The solution is: ', results);
+            
+                 var subject1=results[0].subject1;
+                 var subject2=results[0].subject2;
+                 var subject3=results[0].subject3;
+                 var subject4=results[0].subject4;
+                 var subject5=results[0].subject5;
+            
+            console.log("subjects"+subject1+subject2+subject3);
+            
+            var sql1 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject1+' = ? and student_id ='+sid+';'
+               
+                 db.query(sql1,['P',sid], function (error, results, fields) {
+                   if (error) throw error;
+                   console.log('The solution is: ', results);
+                   console.log(results.length);
+                   finalAns.subp1=results.length;
+            
+                   var sql2 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject2+' <> ? and student_id ='+sid+';'
+               
+                   db.query(sql2,['N'], function (error, results, fields) {
+                     if (error) throw error;
+                     console.log('The solution is: ', results);
+                     console.log(results.length);
+                     finalAns.subp2=results.length;
+            
+                     var sql3 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject3+' <> ? and student_id ='+sid+';'
+               
+                     db.query(sql3,['N'], function (error, results, fields) {
+                       if (error) throw error;
+                       console.log('The solution is: ', results);
+                       console.log(results.length);
+                       finalAns.subp3=results.length;
+            
+                       var sql4 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject4+' <> ? and student_id ='+sid+';'
+               
+                       db.query(sql4,['N'], function (error, results, fields) {
+                         if (error) throw error;
+                         console.log('The solution is: ', results);
+                         console.log(results.length);
+                         finalAns.subp4=results.length;
+            
+                         var sql5 = 'select distinct attendance_date  from  attendance'+Class+' where '+subject5+' <> ? and student_id ='+sid+';'
+               
+                         db.query(sql5,['N'], function (error, results, fields) {
+                           if (error) throw error;
+                           console.log('The solution is: ', results);
+                           console.log(results.length);
+                           finalAns.subp5=results.length;
+
+                           finalAns.suba1=finalAns.sub1-finalAns.subp1;
+                           finalAns.suba2=finalAns.sub1-finalAns.subp2;
+                           finalAns.suba3=finalAns.sub1-finalAns.subp3;
+                           finalAns.suba4=finalAns.sub1-finalAns.subp4;
+                           finalAns.suba5=finalAns.sub1-finalAns.subp5;
+
+            res.send(finalAns);
+                           
+            
+                       })
+                    
+            
+            
+                     })
+            
+            
+                   })
+            
+            
+            
+            
+                 })
+                   
+            
+               })
+            })
+            
+            
+            
+               })
+            
+
+
+
+
+
+
+
+
+
                
 
            })
@@ -206,6 +210,7 @@ res.send(finalAns);
    
 
 })
+
 
 
 
